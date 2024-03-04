982b81daaad96814010576489e13989e7cca3f2c
megatank58 <tanumegatank@gmail.com>
2024-03-04 15:37:02 +0530

    feat: switch to lazt.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,17 @@
-require("config.mappings")
-require("config.options")
-require("config.packer")
-require("config.highlights")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+require("lazy").setup("config/plugins")
+
+

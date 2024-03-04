28646cebe5bfd3a8522a5dbb8549fcff249fde83
megatank58 <tanumegatank@gmail.com>
2024-03-04 16:05:06 +0530

    fix: autorun.sh

--- awesome/autorun.sh
+++ awesome/autorun.sh
@@ -7,10 +7,10 @@ setxkbmap us
 flameshot &
 
 xrandr --output Virtual1 --mode 1920x1080
-picom --config ~/.config/picom/picom.conf &
+#picom --config ~/.config/picom/picom.conf &
 
 #nm-applet &
-feh --bg-scale ~/Wallpapers/wallpaper.jpg &
+#feh --bg-scale ~/Wallpapers/wallpaper.jpg &
 
 # sleep 1
 # volumeicon &
